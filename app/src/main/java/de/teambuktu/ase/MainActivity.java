585e1dee7ef1101c7660347d09ec24a820a247d1
Adrian <adrian.klopfer@kladan.de>
2018-10-13 14:27:02 +0200

    Initiale Regeln werden eingefügt; B -> "-"; A -> Checkboxen
    #6

--- app/src/main/java/de/teambuktu/ase/MainActivity.java
+++ app/src/main/java/de/teambuktu/ase/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int actionCount = 0;
     int conditionCount = 0;
+    int ruleCount = 3;
 
     private void addRow(boolean action) {
         TableLayout table;
@@ -27,21 +29,45 @@ public class MainActivity extends AppCompatActivity {
         EditText column2 = new EditText(this);
 
         if (action) {
-            row.setId(actionCount++);
+            row.setTag("A" + actionCount++);
             column1.setText("A" + actionCount);
         }
         else {
-            row.setId(conditionCount++);
+            row.setTag("C" + conditionCount++);
             column1.setText("B" + conditionCount);
         }
+        column1.setEms(2);
 
         column1.setIncludeFontPadding(true);
         column1.setPadding(10,0,0,0);
         row.addView(column1);
 
         column2.setHint(action ? "Aktion" : "Bedingung");
-        column2.setEms(8);
+        column2.setEms(6);
         row.addView(column2);
+
+        if (action) {
+            CheckBox actionRule;
+            for (int i = 1; i <= ruleCount; i++) {
+                actionRule = new CheckBox(this);
+                actionRule.setTag("A" + (conditionCount - 1) + "-R" + i);
+                actionRule.setEms(2);
+                row.addView(actionRule);
+            }
+        }
+        else {
+            TextView conditionRule;
+            for (int i = 1; i <= ruleCount; i++) {
+                conditionRule = new TextView(this);
+                conditionRule.setTag("C" + (conditionCount - 1) + "-R" + i);
+                conditionRule.setText("-");
+                conditionRule.setEms(2);
+                conditionRule.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                conditionRule.setClickable(true);
+                conditionRule.setFocusable(true);
+                row.addView(conditionRule);
+            }
+        }
 
         table.addView(row);
     }
