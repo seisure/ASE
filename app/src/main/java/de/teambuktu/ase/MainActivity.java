d0b1d1351ef8eacdfe5f5b65777e75391d01eb09
Adrian <acc@kladan.de>
2018-10-23 10:55:06 +0200

    Initial Activity zum schnellen Bearbeiten der Tabelle hinzugefügt; Verbesserte UX (#37)
    #7

--- app/src/main/java/de/teambuktu/ase/MainActivity.java
+++ app/src/main/java/de/teambuktu/ase/MainActivity.java
@@ -2,7 +2,9 @@ package de.teambuktu.ase;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
@@ -21,11 +23,10 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    int ruleCount = 3;
     ArrayList<Action> actionList = new ArrayList<>();
     ArrayList<Condition> conditionList = new ArrayList<>();
 
-    private void addRow(Action actionToAdd) {
+    private void addRowToUI(Action actionToAdd) {
         TableLayout table;
         table = findViewById(R.id.tableAction);
 
@@ -75,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void addRow(final Condition conditionToAdd) {
+    private void addRowToUI(final Condition conditionToAdd) {
         TableLayout table;
         table = findViewById(R.id.tableCondition);
 
@@ -171,14 +172,31 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Action> actions = storageHelper.loadActions();
         this.actionList = actions;
         for (Action action : actions) {
-            addRow(action);
+            addRowToUI(action);
         }
 
         ArrayList<Condition> conditions = storageHelper.loadConditions();
         this.conditionList = conditions;
         for (Condition condition : conditions) {
-            addRow(condition);
+            addRowToUI(condition);
         }
+
+        if (this.actionList.size() == 0 && this.conditionList.size() == 0) {
+            Intent initialIntent = new Intent(this, InitialActivity.class);
+
+            initialIntent.putExtra("rules", getRuleCount());
+            startActivityForResult(initialIntent, 1);
+        }
+    }
+
+    private int getRuleCount() {
+        int rules = 0;
+        if (!conditionList.isEmpty()) {
+            rules = conditionList.get(0).rules.size();
+        } else if (!actionList.isEmpty()) {
+            rules = actionList.get(0).rules.size();
+        }
+        return rules;
     }
 
     @Override
@@ -192,19 +210,34 @@ public class MainActivity extends AppCompatActivity {
         StorageHelper storageHelper = new StorageHelper(this.getApplicationContext());
         switch (item.getItemId()) {
             case R.id.buttonAddActionRow:
-                Action action = new Action(ruleCount);
+                Action action = new Action(getRuleCount());
                 actionList.add(action);
                 storageHelper.update(actionList, conditionList);
-                addRow(action);
+                addRowToUI(action);
                 return true;
             case R.id.buttonAddConditionRow:
-                Condition condition = new Condition(ruleCount);
+                Condition condition = new Condition(getRuleCount());
                 conditionList.add(condition);
                 storageHelper.update(actionList, conditionList);
-                addRow(condition);
+                addRowToUI(condition);
+                return true;
+            case R.id.buttonAddRuleColumn:
+                if (!conditionList.isEmpty()) {
+                    setNumberOfRules(conditionList.get(0).rules.size() + 1);
+                } else if (!actionList.isEmpty()) {
+                    setNumberOfRules(actionList.get(0).rules.size() + 1);
+                }
+                storageHelper.update(actionList, conditionList);
                 return true;
             case R.id.buttonRuleCount:
-                setNumberOfRules();
+                setNumberOfRulesDialog();
+                return true;
+            case R.id.buttonCreatenitialTable:
+                Intent initialIntent = new Intent(this, InitialActivity.class);
+                initialIntent.putExtra("conditions", conditionList.size());
+                initialIntent.putExtra("actions", actionList.size());
+                initialIntent.putExtra("rules", getRuleCount());
+                startActivityForResult(initialIntent, 1);
                 return true;
             case R.id.buttonClearTable:
                 clearTable();
@@ -214,7 +247,67 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setNumberOfRules() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK) {
+            int conditions = data.getIntExtra("conditions", 0);
+            int actions = data.getIntExtra("actions", 0);
+            int rules = data.getIntExtra("rules", 0);
+
+            setNumberOfRules(rules);
+
+            if (conditions > conditionList.size()) {
+                for (int i = 0; i <= conditions - conditionList.size(); i++) {
+                    Condition condition = new Condition(rules);
+                    conditionList.add(condition);
+                    addRowToUI(condition);
+                }
+            } else if (conditions < conditionList.size()) {
+                for (int i = conditionList.size() - 1; i > conditions - 1; i--) {
+                    conditionList.remove(i);
+                    TableLayout tableLayout = findViewById(R.id.tableCondition);
+                    tableLayout.removeViewAt(i);
+                }
+            }
+
+            if (actions > actionList.size()) {
+                for (int i = 0; i <= actions - actionList.size(); i++) {
+                    Action action = new Action(rules);
+                    actionList.add(action);
+                    addRowToUI(action);
+                }
+            } else if (actions < actionList.size()) {
+                for (int i = actionList.size() - 1; i > actions - 1; i--) {
+                    actionList.remove(i);
+                    TableLayout tableLayout = findViewById(R.id.tableAction);
+                    tableLayout.removeViewAt(i);
+                }
+            }
+
+            StorageHelper storageHelper = new StorageHelper(this.getApplicationContext());
+            storageHelper.update(actionList, conditionList);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setNumberOfRules(int count) {
+        TableLayout actionTable = findViewById(R.id.tableAction);
+        actionTable.removeAllViews();
+        for (Action action : actionList) {
+            action.setNumberOfRules(count);
+            addRowToUI(action);
+        }
+
+        TableLayout conditionTable = findViewById(R.id.tableCondition);
+        conditionTable.removeAllViews();
+        for (Condition condition : conditionList) {
+            condition.setNumberOfRules(count);
+            addRowToUI(condition);
+        }
+    }
+
+    private void setNumberOfRulesDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.ruleCount);
 
@@ -227,21 +320,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String text = input.getText().toString();
-                ruleCount = Integer.parseInt(text);
-
-                TableLayout actionTable = findViewById(R.id.tableAction);
-                actionTable.removeAllViews();
-                for (Action action : actionList) {
-                    action.setNumberOfRules(ruleCount);
-                    addRow(action);
-                }
-
-                TableLayout conditionTable = findViewById(R.id.tableCondition);
-                conditionTable.removeAllViews();
-                for (Condition condition : conditionList) {
-                    condition.setNumberOfRules(ruleCount);
-                    addRow(condition);
-                }
+                int ruleCount = Integer.parseInt(text);
+                setNumberOfRules(ruleCount);
 
                 StorageHelper storageHelper = new StorageHelper(context);
                 storageHelper.update(actionList, conditionList);
