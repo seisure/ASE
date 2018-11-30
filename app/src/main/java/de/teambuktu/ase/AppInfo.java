a297dd0dc9eba0c4649cf120a2aa96949ea07045
Michael <administrator@it-michael.de>
2018-11-30 08:28:00 +0100

    adding HowTo-Alert in App-Info via Link #87

--- app/src/main/java/de/teambuktu/ase/AppInfo.java
+++ app/src/main/java/de/teambuktu/ase/AppInfo.java
@@ -1,10 +1,14 @@
 package de.teambuktu.ase;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class AppInfo extends AppCompatActivity {
@@ -44,5 +48,22 @@ public class AppInfo extends AppCompatActivity {
         String versionCode = BuildConfig.VERSION_NAME;
         TextView textView = findViewById(R.id.appInfoVersionNumber);
         textView.setText("Version " + versionCode);
+    }
+
+    public void onClickAppInfoHowTo(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.howToTitle);
+        builder.setMessage(R.string.howToText);
+        final Context context = this.getApplicationContext();
+
+        builder.setPositiveButton(R.string.howToAccept, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                StorageHelper storageHelper = new StorageHelper(context);
+                storageHelper.setInitialStartupFlag(false);
+            }
+        });
+
+        builder.show();
     }
 }
