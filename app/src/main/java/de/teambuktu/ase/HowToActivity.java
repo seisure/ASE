794f5d45ea20ad1c769cb0af1fa19b84eabb72c6
Adrian <acc@kladan.de>
2018-12-04 13:14:58 +0100

    Feature/87 (#111)
    * new menu item App-Info and new activity for displaying App-Info
    #98
    
    * adding in app info: autors of app, app logo and link to project location on github
    #89
    
    * adding version number to app info
    #88
    
    * move content to optical center
    remove outer constraint layout
    change color of app-version to android predefined color instead of own color definition
    use textAppearance property of textView for every textView
    #99 changes
    
    * adding HowTo-Alert in App-Info via Link
    #87
    
    * showHowTo on initialStartup
    #87
    
    * Anpassung an's Mockup
    
    #87
    
    * HowTo-Activity zur Erklaerung der App
    
    #87

---
+++ app/src/main/java/de/teambuktu/ase/HowToActivity.java
@@ -0,0 +1,45 @@
+package de.teambuktu.ase;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class HowToActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to);
+
+        boolean isInitial = getIntent().getExtras().getBoolean("isInitial");
+        if (isInitial) {
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setTitle(R.string.howToTitle);
+            }
+            Button button = findViewById(R.id.buttonFinishAndStart);
+            button.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void finishHowTo(View v) {
+        StorageHelper storageHelper = new StorageHelper(getApplicationContext());
+        storageHelper.setInitialStartupFlag(false);
+        finish();
+    }
+}
