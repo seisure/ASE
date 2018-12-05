6c50496bb23b8a431ef012b060a37650f9ce24ee
thorstenwildberger <it16047@lehre.dhbw-stuttgart.de>
2018-12-05 15:55:33 +0100

    #116 Layout fuer Vorschlaege gefixt

--- app/src/main/java/de/teambuktu/ase/Notification.java
+++ app/src/main/java/de/teambuktu/ase/Notification.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.v4.widget.TextViewCompat;
 import android.view.Gravity;
 import android.widget.TextView;
 
@@ -32,7 +33,7 @@ public class Notification {
         builder.setTitle(String.format("%d %s", suggestion.getSuggestionSize(),
                 context.getResources().getString(R.string.warningDialogTitle)));
 
-        if (suggestion.getSuggestionSize() < 10) {
+        if (suggestion.getSuggestionSize() < 9) {
             builder.setMessage(R.string.warningDialogMessage);
         } else {
             builder.setMessage(context.getResources()
@@ -43,6 +44,7 @@ public class Notification {
         textView.setText(suggestion.toString());
         textView.setGravity(Gravity.CENTER);
         textView.setTextSize(20);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(textView, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
         builder.setView(textView);
         builder.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
