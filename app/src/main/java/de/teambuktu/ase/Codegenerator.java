80ff18c0b08a410994075bcc36bbbd6f8d62b4bd
Michael <administrator@it-michael.de>
2018-12-05 19:00:52 +0100

    bugfix #129

--- app/src/main/java/de/teambuktu/ase/Codegenerator.java
+++ app/src/main/java/de/teambuktu/ase/Codegenerator.java
@@ -7,6 +7,7 @@ class Codegenerator {
     String generateCode(ArrayList<Condition> conditions, ArrayList<Action> actions) {
         StringBuilder codeString = new StringBuilder();
         int ruleCount = Utility.getRuleCount(conditions, actions);
+        String title;
 
         for (int i = 0; i < ruleCount; i++) {
 
@@ -15,31 +16,45 @@ class Codegenerator {
             for (int j = 0; j < conditions.size(); j++) {
                 Condition currentCondition = conditions.get(j);
                 String conditionValue = currentCondition.rules.get(i).getRuleConditionValue();
+                if (currentCondition.getTitle() == null) {
+                    title = "B" + j;
+                } else {
+                    title = currentCondition.getTitle();
+                }
 
                 if (conditionValue.equals("J")) {
-                    codeString.append(String.format("(%s)", currentCondition.getTitle()));
+                    codeString.append(String.format("(%s)", title));
                     if (j < (conditions.size() - 1)) {
-                        codeString.append(String.format(" && ", currentCondition.getTitle()));
+                        codeString.append(" && ");
                     }
                 }
                 if (conditionValue.equals("N")) {
-                    codeString.append(String.format("!(%s)", currentCondition.getTitle()));
+                    codeString.append(String.format("!(%s)", title));
                     if (j < (conditions.size() - 1)) {
-                        codeString.append(String.format(" && ", currentCondition.getTitle()));
+                        codeString.append(" && ");
                     }
                 }
-                if (conditionValue.equals("-")) {
-                    codeString.append(String.format("true", currentCondition.getTitle()));
-                }
+            }
+
+            if (codeString.toString().endsWith("if (")) {
+                codeString.append("true");
+            } else if (codeString.toString().endsWith(" && ")) {
+                codeString.replace(codeString.length() - 4, codeString.length(), "");
             }
 
             codeString.append(") { \n");
 
-            for (Action action : actions) {
-                boolean actionValue = action.rules.get(i).getRuleActionValue();
+            for (int j = 0; j < actions.size(); j++) {
+                Action currentAction = actions.get(j);
+                boolean actionValue = currentAction.rules.get(i).getRuleActionValue();
+                if (currentAction.getTitle() == null) {
+                    title = "A" + j;
+                } else {
+                    title = currentAction.getTitle();
+                }
 
                 if (actionValue == true) {
-                    codeString.append(String.format("%s();", action.getTitle()));
+                    codeString.append(String.format("%s();", title));
                     codeString.append("\n");
                 }
 
